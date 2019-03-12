0d09acb879905ad3d0368afb0a23285454272d32
Victor <victor.spassov@gmail.com>
2019-03-12 13:46:48 +0000

    unless

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,9 +6,8 @@ class Movie < ActiveRecord::Base
   validates :title, :presence => true
   validates :release_date, :presence => true
   validate :released_1930_or_later
-  validates :rating, :inclusion => {:in => Movie.all_ratings }
-    :unless => :grandfathered?
-    
+  # validates :rating, :inclusion => {:in => Movie.all_ratings } unless self.grandfathered?
+  
   def released_1930_or_later
     errors.add(:release_date, 'must be 1930 or later') if 
       release_date && release_date < Date.parse('1 Jan 1930')
