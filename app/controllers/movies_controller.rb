8002b3662d89a8edca2174f5b730b42b2a806e58
Victor <victor.spassov@gmail.com>
2019-03-14 17:57:03 +0000

    session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,10 @@
 class MoviesController < ApplicationController
   def index
-    sort = params[:sort]
+    if !params.key?(:ratings)
+      params[:ratings] = {}
+    end
+    permitted = params.permit(:sort, ratings: params[:ratings].keys)
+    sort = permitted[:sort] || session[:sort]
     
     case sort
     when 'title'
@@ -10,10 +14,16 @@ class MoviesController < ApplicationController
     end
     
     @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings]|| {}
+    @selected_ratings = permitted[:ratings] || session[:ratings] || {}
     
     if @selected_ratings == {}
       @selected_ratings = Hash[@all_ratings.map { |rating| [rating, rating]}]
+    end
+    
+    if permitted[:sort] != session[:sort] or permitted[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
     
     @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
